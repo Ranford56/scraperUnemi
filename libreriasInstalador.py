35c307f50b575e21fb759048159b5657ce880fe5
Ranford56 <67764089+Ranford56@users.noreply.github.com>
2021-02-04 02:14:01 -0500

    Update libreriasInstalador.py

--- libreriasInstalador.py
+++ libreriasInstalador.py
@@ -7,7 +7,7 @@ def packetInstaller(package):
         pip._internal.main(['install', package])
 
 
-libs = ["beautifulsoup4", "requests", "openpyxl", "lxml"]
+libs = ["beautifulsoup4", "requests", "openpyxl", "lxml", "pyqt5", "pyqt5-tools"]
 
 for lib in libs:
     packetInstaller(str(lib))
